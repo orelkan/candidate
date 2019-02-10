ecbbc50f90e8c02a748de4bc1c0f187a0d7b078a
Dor Mesica <dormesica10@gmail.com>
2019-02-10 09:43:28 +0200

    Configured HapiSwagger in the server package.

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,4 +1,15 @@
 const Hapi = require('hapi');
+const Inert = require('inert');
+const Vision = require('Vision');
+const HapiSwagger = require('hapi-swagger');
+const Pack = require('../package');
+
+const swaggerOptions = {
+    info: {
+        title: 'Candidate management API',
+        version: Pack.version,
+    }
+};
 
 const server = Hapi.server({
     port: 80 || process.env.PORT,
@@ -8,6 +19,15 @@ server.route(require('./api/isAlive'));
 
 async function start() {
     try {
+        await server.register([
+            Inert,
+            Vision,
+            {
+                plugin: HapiSwagger,
+                options: swaggerOptions,
+            },
+        ]);
+
         await server.start();
         console.log(`Server running at: ${server.info.uri}`); // eslint-disable-line no-console
     } catch (err) {
