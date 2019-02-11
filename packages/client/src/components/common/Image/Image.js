53d811b5fba37a9d1c99cb67f7cbcb4620d6d9d2
Dor Mesica <dormesica10@gmail.com>
2019-02-11 11:25:34 +0200

    Added bootstrap CSS for index.js. Configured React-Router into Appjs. Implemented Image component with busy indicator. Added stories for Image.js. Configured automatic loading of stories into storybook.

---
+++ packages/client/src/components/common/Image/Image.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+
+import { Spinner } from 'reactstrap';
+
+const Image = React.memo(props => {
+    const [isLoaded, setLoaded] = useState(false);
+    const ref = useRef(null);
+    useEffect(() => {
+        if (ref && ref.current) {
+            if (ref.current.complete) {
+                handleImageLoaded();
+            } else {
+                ref.current.addEventListener('load', handleImageLoaded);
+            }
+
+            return () => ref.current.removeEventListener('load', handleImageLoaded);
+        }
+    }, [props.src]);
+
+    function handleImageLoaded() {
+        setLoaded(true);
+    }
+
+    const { src, className, spinnerSize } = props;
+
+    return (
+        <div className={className} style={{ display: 'flex', width: '500px', height: '500px' }}>
+            <img
+                ref={ref}
+                src={src}
+                alt="Not Found"
+                style={{ display: isLoaded ? 'block' : 'none', width: '100%', height: '100%' }}
+            />
+            {!isLoaded ? (
+                <Spinner style={{ margin: 'auto', width: spinnerSize, height: spinnerSize }} color="info" />
+            ) : null}
+        </div>
+    );
+});
+
+Image.propTypes = {
+    src: PropTypes.string.isRequired,
+    className: PropTypes.string,
+    spinnerSize: PropTypes.number,
+};
+
+export default Image;
