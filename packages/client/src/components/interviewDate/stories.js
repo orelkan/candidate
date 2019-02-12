461dc1e55dfe9ab9e39ff03e8e8920c22ca75330
Bar7 <baryaacovi7@gmail.com>
2019-02-12 16:22:31 +0200

    Changed name of folder to fit conventions

--- packages/client/src/components/interviewDate/stories.js
+++ packages/client/src/components/interviewDate/stories.js
@@ -2,7 +2,7 @@ import { storiesOf } from '@storybook/react';
 import React from 'react';
 import InterviewDate from './interviewDate'
 
-storiesOf('InterviewLocation', module)
+storiesOf('interviewLocation', module)
     .add('dateViewer', () => (
         <InterviewDate
             date = {new Date()}
