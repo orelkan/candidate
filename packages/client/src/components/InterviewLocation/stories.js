8cdc39692415d21da366d425c535ec7693970d83
Bar7 <baryaacovi7@gmail.com>
2019-02-12 16:20:03 +0200

    Fixed a bug with stories

--- packages/client/src/components/InterviewLocation/stories.js
+++ packages/client/src/components/InterviewLocation/stories.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import InterviewLocation from './interviewLocation';
 
-storiesOf.add('location', () =>(
+storiesOf('InterviewLocation', module)
+    .add('location', () =>(
     <InterviewLocation
         location={"גלילות"}
         imgSrc={"https://cdn.bulbagarden.net/upload/thumb/0/0d/025Pikachu.png/1200px-025Pikachu.png"} //temp img
