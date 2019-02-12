8ef6ce2d505f28ee5055a4bf3e64ffa5ceb84104
Ahuvim <37769444+Ahuvim@users.noreply.github.com>
2019-02-12 13:15:40 +0200

    Server (#7)
    added a path for all reviewers.
    added path for reviewer by ID.
    added reviewers to index.js.

---
+++ packages/server/src/api/reviewers/getByID.js
@@ -0,0 +1,32 @@
+const Joi = require('joi');
+const { reviewerScheme } = require('./reviewersSchema');
+const dal = require('../../DAL/reviewers');
+
+const get = {
+    method: 'GET',
+    path: '/reviewers/{id}',
+    options: {
+        description: 'Get interviewer details',
+        notes: "Returns the interviewer's information which was specified by id",
+        tags: ['api', 'interviewer'],
+        validate: {
+            params: {
+                id: Joi.string()
+                    .regex(/[0-9]+/)
+                    .length(7)
+                    .trim(true)
+                    .truncate(true),
+            },
+        },
+        response: {
+            schema: Joi.array().items(reviewerScheme),
+        },
+    },
+    handler,
+};
+function handler(request) {
+    const returnValue = dal.filter(interviewer => interviewer.id === request.params.id);
+    return returnValue;
+}
+
+module.exports = get;
